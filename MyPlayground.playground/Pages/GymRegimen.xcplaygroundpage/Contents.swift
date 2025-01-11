bcea0a35cf6da3db1dfed240d487558bed663120
AlbertoJTD <albertojtd@outlook.com>
2025-01-11 11:19:46 -0600

    Track gym exercises

---
+++ MyPlayground.playground/Pages/GymRegimen.xcplaygroundpage/Contents.swift
@@ -0,0 +1,57 @@
+//: [Previous](@previous)
+
+import Foundation
+
+struct Exercise {
+  var name: String
+  var muscleGroups: [String]
+  var reps: Int
+  var sets: Int
+  var totalReps: Int
+
+  init(name: String, muscleGroups: [String], reps: Int, sets: Int) {
+    self.name = name
+    self.muscleGroups = muscleGroups
+    self.reps = reps
+    self.sets = sets
+    self.totalReps = reps * sets
+  }
+}
+
+var pushUp = Exercise(name: "Push up", muscleGroups: ["Triceps", "Chest", "Shoulders"], reps: 10, sets: 3)
+// print(pushUp)
+var squats = Exercise(name: "Squats", muscleGroups: ["Gluteus", "Quadriceps femoris"], reps: 10, sets: 3)
+
+
+struct Regimen {
+  var dayOfWeek: String
+  var exercises: [Exercise]
+  
+  init(dayOfWeek: String, exercises: [Exercise]) {
+    self.dayOfWeek = dayOfWeek
+    self.exercises = exercises
+  }
+
+  func printExercisePlan() {
+    print("Today is \(self.dayOfWeek) and the plan is to: ")
+    for exercise in self.exercises {
+      print("Do \(exercise.sets) sets of \(exercise.reps) \(exercise.name)s")
+      print("That's a total of \(exercise.totalReps) \(exercise.name).")
+    }
+  }
+
+  mutating func addExercises(_ exercises: [Exercise]) {
+    self.exercises += exercises
+  }
+}
+
+var mondayRegimen = Regimen(dayOfWeek: "Monday", exercises: [pushUp])
+// print(mondayRegimen)
+mondayRegimen.printExercisePlan()
+
+var tuesdayRegimen = Regimen(dayOfWeek: "Tuesdat", exercises: [squats])
+
+mondayRegimen.addExercises([squats])
+print(mondayRegimen)
+
+
