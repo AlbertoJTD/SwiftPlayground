af2b8866d9a6a518024b97af1f406d182126436f
AlbertoJTD <albertojtd@outlook.com>
2025-01-25 16:32:07 -0600

    Add spaces and validate positive deposits or withdrawals

--- MyPlayground.playground/Pages/SwiftBank.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/SwiftBank.xcplaygroundpage/Contents.swift
@@ -3,71 +3,94 @@
 import Foundation
 
 struct SwiftBank {
-  static let depositBonusRate: Double = 0.01
+    static let depositBonusRate: Double = 0.01
+    private var depositCounter: Int = 0
+    private let password: String
 
-  private let password: String
-
-  // Add observer to check balance property
-  private var balance: Double = 0.0 {
-    didSet {
-      if self.balance <= 100 {
-        displayLowBalanceMessage()
-      }
+    // Add observer to check balance property
+    private var balance: Double = 0.0 {
+        didSet {
+          if self.balance <= 100 {
+            displayLowBalanceMessage()
+          }
+        }
     }
-  }
 
-  init(password: String, initialDeposit: Double) {
+    init(password: String, initialDeposit: Double) {
     self.password = password
     makeDeposit(ofAmount: initialDeposit)
-  }
-
-  // Using 'mutating' because we are overwriting 'balance' property
-  mutating func makeDeposit(ofAmount depositAmount: Double) {
-    let depositWithBonus = finalDepositWithBonus(fromInitialDeposit: depositAmount)
-    print("Making a deposit of $\(depositAmount) with a bonus rate. The final amount deposited is $\(depositWithBonus).")
-    self.balance += depositWithBonus
-  }
-
-  func displayBalance(usingPassword password: String) {
-    let validPassword = isValid(password)
-
-    if validPassword {
-      print("Your current balance is $\(self.balance)")
-    } else {
-      print("Error: Invalid password. Cannot retrieve balance.")
-      return
     }
-  }
 
-  // Using 'mutating' because we are overwriting 'balance' property
-  mutating func makeWithdrawal(ofAmount withdrawalAmount: Double, usingPassword password: String) {
-    let validPassword = isValid(password)
+    // Using 'mutating' because we are overwriting 'balance' property
+    mutating func makeDeposit(ofAmount depositAmount: Double) {
+        if !isValidAmount(of: depositAmount) {
+            print("Operation not valid, deposit amount must be positive.")
+            return
+        }
+        
+        if depositAmount >= 1000 {
+            self.depositCounter += 1
+        }
 
-    if validPassword {
-      if self.balance >= withdrawalAmount {
-        self.balance -= withdrawalAmount
-        print("Making a $\(withdrawalAmount) withdrawal")
-      } else {
-        print("You don't have enought money to make a withdrawal of $\(withdrawalAmount)")
-      }
-    } else {
-      print("Error: Invalid password. Cannot make withdrawal.")
-      return
+        let depositWithBonus = finalDepositWithBonus(fromInitialDeposit: depositAmount)
+        print("Making a deposit of $\(depositAmount) with a bonus rate. The final amount deposited is $\(depositWithBonus).")
+        self.balance += depositWithBonus
     }
-  }
 
-  private func isValid(_ enteredPassword: String) -> Bool {
-    return self.password == enteredPassword
-  }
+    func displayBalance(usingPassword password: String) {
+        let validPassword = isValid(password)
 
-  private func finalDepositWithBonus(fromInitialDeposit deposit: Double) -> Double {
-    let bonus = deposit * SwiftBank.depositBonusRate
-    return deposit + bonus
-  }
+        if validPassword {
+          print("Your current balance is $\(self.balance)")
+        } else {
+          print("Error: Invalid password. Cannot retrieve balance.")
+          return
+        }
+    }
 
-  private func displayLowBalanceMessage() {
-    print("Alert: Your balance is under $100.")
-  }
+    // Using 'mutating' because we are overwriting 'balance' property
+    mutating func makeWithdrawal(ofAmount withdrawalAmount: Double, usingPassword password: String) {
+        let validPassword = isValid(password)
+
+        if validPassword {
+            if !isValidAmount(of: withdrawalAmount) {
+                print("Operation not valid, withdrawal amount must be positive.")
+                return
+            }
+            
+            if self.balance >= withdrawalAmount {
+                self.balance -= withdrawalAmount
+                print("Making a $\(withdrawalAmount) withdrawal")
+            } else {
+                print("You don't have enought money to make a withdrawal of $\(withdrawalAmount)")
+            }
+        } else {
+            print("Error: Invalid password. Cannot make withdrawal.")
+            return
+        }
+    }
+
+    private func isValid(_ enteredPassword: String) -> Bool {
+        return self.password == enteredPassword
+    }
+
+    private func finalDepositWithBonus(fromInitialDeposit deposit: Double) -> Double {
+        if self.depositCounter == 1 {
+            print("It's your first deposit! You get a 1% bonus.")
+            let bonus = deposit * SwiftBank.depositBonusRate
+            return deposit + bonus
+        } else {
+            return deposit
+        }
+    }
+
+    private func displayLowBalanceMessage() {
+        print("Alert: Your balance is under $100.")
+    }
+    
+    private func isValidAmount(of amount: Double) -> Bool {
+        return amount > 0
+    }
 }
 
 
@@ -76,5 +99,4 @@ myAccount.makeDeposit(ofAmount: 50)
 myAccount.makeWithdrawal(ofAmount: 100, usingPassword: "12345")
 myAccount.makeWithdrawal(ofAmount: 100, usingPassword: "123456")
 myAccount.displayBalance(usingPassword: "123456")
-
 
